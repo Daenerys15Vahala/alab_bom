21bbd29950363d7c3a016391b66f50d2f8d039ae
Valentina BM <valentina15bermo@gmail.com>
2026-07-14 21:02:22 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-// Guess the Movie with the Emojis
+// ALAB DOM - GUESSING GAME
 
+// movie list
+
+const welcomeText = "Guess the movie with emojis"
+alert(welcomeText);
+
+const movies = ["🦖🌎🔬🧬", "🐀🍝👨🏻‍🍳" ,"🎈🤡", "🏃‍♂️🏓🪖🍫","🐯👑🐗", "🧍🏼‍♂️❄️🎄🏠", "🪨❤️🚢", "🏰🤓⚡️", "🦇🤡", "🧙‍♂️💍🌋"]
+
+// random movie
+
+// show emojis
+
+// wait for guess
+
+// compare guess
+
+// show message
+
+// update scene
+
+// play again
