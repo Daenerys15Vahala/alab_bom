4113dcb14a2dd260e659c27e5cea6aee01343f42
Valentina BM <valentina15bermo@gmail.com>
2026-07-14 21:22:47 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,37 @@
 const welcomeText = "Guess the movie with emojis"
 alert(welcomeText);
 
-const movies = ["🦖🌎🔬🧬", "🐀🍝👨🏻‍🍳" ,"🎈🤡", "🏃‍♂️🏓🪖🍫","🐯👑🐗", "🧍🏼‍♂️❄️🎄🏠", "🪨❤️🚢", "🏰🤓⚡️", "🦇🤡", "🧙‍♂️💍🌋"]
+const moviesList = {
+    title: "Jurassic Park",
+    hint: "🦖🌎🔬🧬", 
+    
+    title: "Ratatouille" ,
+    hint: "🐀🍝👨🏻‍🍳",
+    
+    title: "It",
+    hint: "🎈🤡", 
+    
+    title: "Forrest Gump",
+    hint: "🏃‍♂️🏓🪖🍫",
+    
+    title: "The Lion King",
+    hint: "🐯👑🐗", 
+    
+    title: "Home Alone",
+    hint: "🧍🏼‍♂️❄️🎄🏠", 
+    
+    title: "Titanic",
+    hint: "🪨❤️🚢",
+    
+    title: "Harry Potter",
+    hint: "🏰🤓⚡️", 
+    
+    title: "Batman",
+    hint: "🦇🤡", 
+    
+    title: "Lord of the Rings",
+    hint: "🧙‍♂️💍🌋"
+};
 
 // random movie
 
